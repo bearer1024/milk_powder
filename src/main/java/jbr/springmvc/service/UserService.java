e0984bcbf51ee0f4a959eec6fa2c8b7914090651
bearer1024 <bearer1024@gamil.com>
2018-02-10 20:40:20 +0800

    first commit

---
+++ src/main/java/jbr/springmvc/service/UserService.java
@@ -0,0 +1,15 @@
+package jbr.springmvc.service;
+
+import jbr.springmvc.model.Login;
+import jbr.springmvc.model.User;
+
+public class UserService {
+
+   public User validateUser(Login login){
+        User user = new User();
+    return user;
+    }
+   public void register(User user){
+
+    }
+}
